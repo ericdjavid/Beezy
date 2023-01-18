ab74b1905d19a76a89ccf24815b82991eea8bb7a
Eric Djavid <edjavid@student.42.fr>
2023-01-18 01:23:15 +0100

    generating first bundle, connecting geo map with SNCF api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
   <GoogleOAuthProvider clientId="227564136748-9t9ao6jgcit2b62mj3cf7nv2pb4dtcki.apps.googleusercontent.com">
+  <Toaster />
   <Component {...pageProps}/>
   </GoogleOAuthProvider>
   </>
